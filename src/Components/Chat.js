5e10892c664e07c4b47feb2e3cbf0542aec3e8a5
Robert Gregg <hi@robertgregg.co.uk>
2021-01-23 14:34:37 +0000

    Reorganised files. Added Chat component

--- src/Components/Chat.js
+++ src/Components/Chat.js
@@ -3,6 +3,8 @@ import "../css/Chat.css";
 import { Avatar, IconButton } from "@material-ui/core";
 import { AttachFile, SearchOutlined } from "@material-ui/icons";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
+import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
+import MicIcon from "@material-ui/icons/Mic";
 
 const Chat = () => {
   return (
@@ -24,6 +26,32 @@ const Chat = () => {
             <MoreVertIcon />
           </IconButton>
         </div>
+      </div>
+
+      <div className="chat__body">
+        <p className="chat__message">
+          <span className="chat__name">Robby G</span>
+          <span>This is a message</span>
+          <span className="chat__timestamp">{new Date().toUTCString()}</span>
+        </p>
+        <p className="chat__message chat__receiver">
+          <span className="chat__name">Robby G</span>
+          <span>This is a message</span>
+          <span className="chat__timestamp">{new Date().toUTCString()}</span>
+        </p>
+        <p className="chat__message">
+          <span className="chat__name">Robby G</span>
+          <span>This is a message</span>
+          <span className="chat__timestamp">{new Date().toUTCString()}</span>
+        </p>
+      </div>
+      <div className="chat__footer">
+        <InsertEmoticonIcon />
+        <form>
+          <input type="text" placeholder="Type a message" />
+          <button type="submit">Send a message</button>
+        </form>
+        <MicIcon />
       </div>
     </div>
   );
