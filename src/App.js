56901ea805721daf3d0353135ed89e461ee99f86
Robert Gregg <hi@robertgregg.co.uk>
2021-01-23 21:43:18 +0000

    PUSHER all working

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Sidebar from "./Components/Sidebar";
 import Chat from "./Components/Chat";
 import "./css/App.css";
+import Pusher from "pusher-js";
 
 function App() {
+  useEffect(() => {
+    const pusher = new Pusher("fd63c2a6a3729ad1a8a0", {
+      cluster: "eu",
+    });
+
+    const channel = pusher.subscribe("messages");
+    channel.bind("inserted", (data) => {
+      alert(JSON.stringify(data));
+    });
+  }, []);
+
   return (
     <div className="app">
       <div className="app__body">
