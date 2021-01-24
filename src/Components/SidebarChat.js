033f4893f1449b3afc20e9848412267b45df97db
Robert Gregg <hi@robertgregg.co.uk>
2021-01-24 21:56:09 +0000

    Added custom random avatars

--- src/Components/SidebarChat.js
+++ src/Components/SidebarChat.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Avatar } from "@material-ui/core";
 import "../css/SidebarChat.css";
 
 export const SidebarChat = () => {
+  const [seed, setSeed] = useState("");
+
+  useEffect(() => {
+    setSeed(Math.floor(Math.random() * 500));
+  }, []);
+
   return (
     <div className="sidebarChat">
-      <Avatar />
+      <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
       <div className="sidebarChat__info">
         <h2>Room Name</h2>
         <p>Last message posted</p>
